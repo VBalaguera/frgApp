ed5239564398b86827487687ebb7096a969ff467
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2020-12-29 20:44:36 +0100

    heroku testing

--- src/pages/posts/posts.component.jsx
+++ src/pages/posts/posts.component.jsx
@@ -7,8 +7,6 @@ import './posts.styles.scss';
 import PublicationsOverview from '../../components/publications-overview/publications-overview.component'; 
 import PublicationPage from '../../pages/publication/publication.component'; 
 
-import PostsJson from '../../components/posts-json/posts.json.component'; 
-
 const PostsPage = ({ match }) => {
     console.log(match); 
     return (
@@ -17,7 +15,6 @@ const PostsPage = ({ match }) => {
                 <Route exact path={`${match.path}`} component={PublicationsOverview} /> 
                 <Route path={`${match.path}/:publicationId`} component={PublicationPage} /> 
             </div>
-            <PostsJson /> 
         </div>
     );
 }
